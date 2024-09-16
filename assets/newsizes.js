e4fccbad2be90498c19eba6006eb4dbddf14c030
MMSG97 <maria.m.mga88@gmail.com>
2024-09-17 00:13:20 +0200

    finition de mkise en place de balises sémantiques + redimenssion des img + convertion en fichiers.webp

---
+++ assets/newsizes.js
@@ -0,0 +1,63 @@
+// const sharp = require("sharp");
+
+// const slide1 = "./images/slider/manInASuit.jpg";
+
+// const slide2 = "./images/slider/concertCrowd.jpg";
+
+// const slide3 = "./images/slider/mariage.jpg";
+
+// //source des dossiers de dépôt
+// const f1920 = "./images/slider/1920";
+// const f1440 = "./images/slider/1440";
+// const f1024 = "./images/slider/1024";
+// const f768 = "./images/slider/768";
+// const f425 = "./images/slider/425";
+// const f375 = "./images/slider/375";
+// const f320 = "./images/slider/320";
+
+// //Redimension à 1920px 1440px, 1024px, 768px, 425px,375px, 320px (L)
+// function redemension(sourceImg, sourceDepot, L, H) {
+//   sharp(sourceImg)
+//     .resize(L, H)
+//     .tofile(sourceDepot, (err, info) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         console.log(info);
+//       }
+//     });
+// }
+
+// redemension(slide1, f1920, 1920, 888);
+
+// // ////////////////////
+// // //sources des images du carousel
+// // const slide1 = "./images/slider/manInASuit.jpg";
+
+// // const slide2 = "./images/slider/concertCrowd.jpg";
+
+// // const slide3 = "./images/slider/mariage.jpg";
+
+// // //source des dossiers de dépôt
+// // const f1920 = "./images/slider/1920";
+// // const f1440 = "./images/slider/1440";
+// // const f1024 = "./images/slider/1024";
+// // const f768 = "./images/slider/768";
+// // const f425 = "./images/slider/425";
+// // const f375 = "./images/slider/375";
+// // const f320 = "./images/slider/320";
+
+// // //Redimension à 1920px 1440px, 1024px, 768px, 425px,375px, 320px (L)
+// // function redemension(sourceImg, sourceDepot, L, H) {
+// //   sharp(sourceImg)
+// //     .resize(L, H)
+// //     .tofile(sourceDepot, (err, info) => {
+// //       if (err) {
+// //         console.error(err);
+// //       } else {
+// //         console.log(info);
+// //       }
+// //     });
+// // }
+
+// // console.log(f1024);
